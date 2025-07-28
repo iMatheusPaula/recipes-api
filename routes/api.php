1f444d119eddd837d3f9d663c694b79ab19969a3
iMatheusPaula <imatheuspaula@gmail.com>
2025-07-28 15:51:47 -0300

    feat: add `RecipePolicy` and routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -7,3 +8,14 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::prefix('recipes')->controller(RecipeController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{recipe}', 'show');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/', 'store');
+        Route::put('/{recipe}', 'update')->middleware('can:update,recipe');
+        Route::delete('/{recipe}', 'destroy')->middleware('can:delete,recipe');
+    });
+});
