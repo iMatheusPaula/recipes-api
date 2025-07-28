4d67246dd8f4e63d18fa5df1e3a25d935644b126
iMatheusPaula <imatheuspaula@gmail.com>
2025-07-28 15:50:59 -0300

    feat: add `RecipeController` with CRUD and `CreateRecipeRequest` and `UpdateRecipeRequest` for request validation.

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateRecipeRequest;
+use App\Http\Requests\UpdateRecipeRequest;
+use App\Models\Recipe;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+class RecipeController extends Controller
+{
+    /**
+     * Display a listing of recipes.
+     */
+    public function index(): JsonResponse
+    {
+        $recipes = Recipe::all();
+
+        return response()->json($recipes);
+    }
+
+    /**
+     * Store a newly created recipe
+     *
+     * @param CreateRecipeRequest $request
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function store(CreateRecipeRequest $request): JsonResponse
+    {
+        try {
+            $data = $request->validated();
+
+            $recipe = Recipe::query()->create([
+                ...$data,
+                'user_id' => auth()->id(),
+            ]);
+
+            return response()->json($recipe, 201);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Display the specified recipe.
+     */
+    public function show(Recipe $recipe): JsonResponse
+    {
+        try {
+            return response()->json($recipe);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Update the specified recipe in storage.
+     */
+    public function update(UpdateRecipeRequest $request, Recipe $recipe): JsonResponse
+    {
+        try {
+            $data = $request->validated();
+
+            $recipe->update($data);
+
+            return response()->json($recipe);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Remove the specified recipe from storage.
+     */
+    public function destroy(Recipe $recipe): JsonResponse
+    {
+        try {
+            $recipe->delete();
+
+            return response()->json(['message' => 'Recipe deleted successfully'], 204);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
---
+++ app/Http/Requests/CreateRecipeRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRecipeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'ingredients' => 'required|string',
+            'instructions' => 'required|string'
+        ];
+    }
+
+    /**
+     * Get custom error messages for the validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O nome da receita é obrigatório.',
+            'name.string' => 'O nome da receita deve ser uma string.',
+            'name.max' => 'O nome da receita não pode exceder 255 caracteres.',
+            'description.required' => 'A descrição da receita é obrigatória.',
+            'description.string' => 'A descrição da receita deve ser uma string.',
+            'ingredients.required' => 'Os ingredientes da receita são obrigatórios.',
+            'ingredients.string' => 'Os ingredientes da receita devem ser uma string.',
+            'instructions.required' => 'As instruções da receita são obrigatórias.',
+            'instructions.string' => 'As instruções da receita devem ser uma string.'
+        ];
+    }
+}
