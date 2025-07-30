d5a378672cc351a34f0eabfcebb907feacd7e18a
iMatheusPaula <imatheuspaula@gmail.com>
2025-07-29 22:42:50 -0300

    feat: add factories for Recipe and Review

---
+++ database/factories/RecipeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Recipe>
+ */
+class RecipeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->text(30),
+            'description' => fake()->text(),
+            'ingredients' => fake()->text(),
+            'instructions' => fake()->text(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
